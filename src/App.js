6319d42060fad2b225f010d2549c397751039d01
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-04-22 11:28:19 +0100

    checking

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className='App'>hello world</div>;
+  const [count, setCount] = useState(0);
+
+  function incr() {
+    setCount(count + 1);
+  }
+  function dcr() {
+    setCount(count - 1);
+  }
+  return (
+    <div className='App'>
+      <button onClick={incr}>increment</button>
+      <h1>{count}</h1>
+      <button onClick={dcr}>decrement</button>
+      <h1>The number is now {count}</h1>
+    </div>
+  );
 }
 
 export default App;
