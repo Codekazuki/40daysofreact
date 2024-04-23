62cd93671cdb066af55592ddf0928b55854e32c6
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-04-23 11:03:15 +0100

    added boiler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Test from "./Testing/Test";
 import ErrorExample from "./tutorial/01-useState/starter/01-error-example";
+import UseRefBasics from "./tutorial/07-useRef/starter/01-useRef-basics";
 
 function App() {
   return (
     <div className='App'>
       <Test />
       <ErrorExample />
+      <UseRefBasics />
     </div>
   );
 }
