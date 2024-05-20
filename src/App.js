8e66580e42634c15c2141eedec35f90755ab1a3c
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-05-20 10:40:27 +0100

    tailwind starter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Test from "./Testing/Test";
 import ErrorExample from "./tutorial/01-useState/starter/01-error-example";
-import UseRefBasics from "./tutorial/07-useRef/starter/01-useRef-basics";
+import UseStatesBasics from "./tutorial/01-useState/starter/02-useState-basics";
 
 function App() {
   return (
     <div className='App'>
+      <UseStatesBasics />
       <Test />
       <ErrorExample />
-      <UseRefBasics />
     </div>
   );
 }
