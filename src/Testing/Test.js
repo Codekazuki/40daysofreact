d08534f57c1c9eaa36192800d9529621b0c3cae7
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-05-22 23:46:38 +0100

    innoliz class

--- src/Testing/Test.js
+++ src/Testing/Test.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./test.css";
 const Test = () => {
   return (
@@ -10,6 +10,13 @@ const Test = () => {
 };
 
 const Hello = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    });
+  }, []);
   return <h1>hello</h1>;
 };
 
