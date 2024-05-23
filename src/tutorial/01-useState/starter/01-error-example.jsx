fba150ff857f80f62aece04ced6068429e3602eb
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-05-23 10:58:45 +0100

    innoliz class

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,13 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const handleClick = () => {
+    console.log("im clicked");
+  };
+  return (
+    <>
+      <button onClick={handleClick}>click</button>
+      <h2>useState error example</h2>
+    </>
+  );
 };
 
 export default ErrorExample;
