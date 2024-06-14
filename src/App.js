5be79777e1e18d7c1dcc95cc8013b4ad93412cc5
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-06-14 09:00:11 +0100

    api

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className='App'>
       <Test />
       <UseStatesBasics />
-      <ErrorExample />
+      {/* <ErrorExample /> */}
     </div>
   );
 }
--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,9 +1,22 @@
+import { useState, useEffect } from "react";
+import Axios from "axios";
+
 const UseStateBasics = () => {
+  const [catFact, setCatFact] = useState("");
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
+  };
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+
   return (
     <>
       <h1>useState basics</h1>
-      <h2>hello</h2>
-      <img src='' alt='' />
+      <button onClick={fetchCatFact}>Generate CatFact</button>
+      <h3>{catFact}</h3>
     </>
   );
 };
