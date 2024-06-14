fbbbe9035778033997356129f375dcf134f3bc25
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-06-14 10:54:19 +0100

    api

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -24,9 +24,10 @@ const UseStateBasics = () => {
     });
   };
   const fetchExcuse = (excuse) => {
-    Axios.get(`https://excuser.herokuapp.com/v1/excuse/${excuse}/`).then(
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(
       (res) => {
         setExcuse(res.data[0].excuse);
+        console.log(res.data);
       }
     );
   };
@@ -51,7 +52,9 @@ const UseStateBasics = () => {
       <button onClick={() => fetchExcuse("family")}>Family</button>
       <button onClick={() => fetchExcuse("party")}>Party</button>
       <button onClick={() => fetchExcuse("office")}>Office</button>
-      <p>{excuse}</p>
+      <button onClick={() => fetchExcuse("developers")}>Developers</button>
+
+      <p> {excuse} </p>
     </>
   );
 };
