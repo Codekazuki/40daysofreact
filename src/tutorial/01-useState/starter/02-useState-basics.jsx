ec2a1b4e7d5419e6fd1a9ad249d20a7391e25fa9
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-06-14 09:45:28 +0100

    api

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import Axios from "axios";
 
 const UseStateBasics = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [count, setCount] = useState(0);
   const [catFact, setCatFact] = useState("");
   const fetchCatFact = () => {
     Axios.get("https://catfact.ninja/fact").then((res) => {
@@ -12,11 +15,30 @@ const UseStateBasics = () => {
     fetchCatFact();
   }, []);
 
+  const getDataPerson = () => {
+    Axios.get(`https://api.agify.io?name=${name}`).then((res) => {
+      setName(res.data.name);
+      setAge(res.data.age);
+      setCount(res.data.count);
+    });
+  };
+
   return (
     <>
       <h1>useState basics</h1>
       <button onClick={fetchCatFact}>Generate CatFact</button>
       <h3>{catFact}</h3>
+      <input
+        type='text'
+        placeholder='Input your first name here'
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <button onClick={getDataPerson}>Predict Age</button>
+      <h1>
+        {name} is {age} years old after reviewing {count} data
+      </h1>
     </>
   );
 };
