906a69321cb216570ba843f34f62c263b5e5d4ab
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-08-12 23:09:36 +0100

    sprint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
 import Test from "./Testing/Test";
-import ErrorExample from "./tutorial/01-useState/starter/01-error-example";
-import UseStatesBasics from "./tutorial/01-useState/starter/02-useState-basics";
 
 function App() {
   return (
     <div className='App'>
       <Test />
-      <UseStatesBasics />
-      {/* <ErrorExample /> */}
     </div>
   );
 }
--- src/Testing/Test.js
+++ src/Testing/Test.js
@@ -1,39 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./test.css";
+
 const Test = () => {
-  const handleClick = () => {
-    console.log("I have been clicked");
-    console.log("Still on Clicking");
-  };
-  return (
-    <>
-      <button onClick={handleClick}>Click me</button>
-
-      <Hello />
-      <h1 className='test'>Testing</h1>
-    </>
-  );
-};
-
-const Hello = () => {
-  const [count, setCount] = useState(0);
-  const handleAddClick = () => {
-    setCount((c) => c + 1);
-  };
-  const handleLessClick = () => {
-    setCount((c) => c - 1);
-  };
-  return (
-    <div className='count'>
-      <button onClick={handleAddClick} className='button'>
-        Increment
-      </button>
-      <span className='num'>{count}</span>
-      <button className='button' onClick={handleLessClick}>
-        Decrement
-      </button>
-    </div>
-  );
+  return <div>Test</div>;
 };
 
 export default Test;
