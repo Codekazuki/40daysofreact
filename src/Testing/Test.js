1ac58577e24d00d5bc2c2f3594198fdc5087567d
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-08-12 23:16:31 +0100

    react sprint

--- src/Testing/Test.js
+++ src/Testing/Test.js
@@ -2,7 +2,25 @@ import React from "react";
 import "./test.css";
 
 const Test = () => {
-  return <div>Test</div>;
+  return (
+    <div>
+      <BigTeam />
+    </div>
+  );
+};
+
+const Club = () => {
+  return <h1>Chelsea</h1>;
+};
+const Slogan = () => <h2>We are back</h2>;
+
+const BigTeam = () => {
+  return (
+    <>
+      <Club />
+      <Slogan />
+    </>
+  );
 };
 
 export default Test;
