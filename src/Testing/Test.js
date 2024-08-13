8fdd7cc3bec087c95ff0d57a4589cd0ba2227abc
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-08-13 08:55:25 +0100

    react sprint

--- src/Testing/Test.js
+++ src/Testing/Test.js
@@ -9,10 +9,30 @@ const Test = () => {
   );
 };
 
+const date = new Date();
+const time = date.getHours();
+let greetingTime;
+let attach;
+if (time < 12) {
+  greetingTime = "good morning";
+  attach = "am";
+} else if (time > 12 && time < 17) {
+  greetingTime = "good evening";
+  attach = "pm";
+} else {
+  greetingTime = "goodnigt";
+  attach = "pm";
+}
+
 const Club = () => {
-  return <h1>Chelsea</h1>;
+  return (
+    <h1>
+      {greetingTime} its {time === 12 ? 12 : time}
+      {attach} already
+    </h1>
+  );
 };
-const Slogan = () => <h2>We are back</h2>;
+const Slogan = () => <h2>We are back </h2>;
 
 const BigTeam = () => {
   return (
