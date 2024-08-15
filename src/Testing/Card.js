b1c539102905973a4ebf4931e1ac16d28996c204
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-08-15 14:48:08 +0100

    sprint

--- src/Testing/Card.js
+++ src/Testing/Card.js
@@ -1,17 +1,20 @@
 import React from "react";
+import pix from "../images/pix.png";
+import phone from "../images/call.png";
+import agent from "../images/agent.png";
+import "./test.css";
 
 const Card = () => {
   return (
-    <section>
-      <img src='./images/my picturepng.png' alt='pix' />
+    <section className='card'>
+      <img src={pix} className='cardImage' alt='pix' />
       <h1>Temitope Agbowuro</h1>
       <div>
-        <img src='' alt='' />
-
+        <img src={phone} alt='call' />
         <p>08069477780</p>
       </div>
       <div>
-        <img src='/my picturepng.png' alt='pix' />
+        <img src={agent} alt='agent' />
         <p>12345678910</p>
       </div>
       <div></div>
