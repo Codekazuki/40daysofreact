c5488ec285c815bfe722ab859c70eb5da328ff54
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-08-15 14:23:15 +0100

    sprint

--- src/Testing/Card.js
+++ src/Testing/Card.js
@@ -3,14 +3,15 @@ import React from "react";
 const Card = () => {
   return (
     <section>
-      <img src='' alt='' />
+      <img src='./images/my picturepng.png' alt='pix' />
       <h1>Temitope Agbowuro</h1>
       <div>
         <img src='' alt='' />
+
         <p>08069477780</p>
       </div>
       <div>
-        <img src='' alt='' />
+        <img src='/my picturepng.png' alt='pix' />
         <p>12345678910</p>
       </div>
       <div></div>
