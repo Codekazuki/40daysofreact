11f872a3a6a28b67318c55e63452180962a912e8
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-08-15 10:22:40 +0100

    sprint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Card from "./Testing/Card";
 import Test from "./Testing/Test";
 
 function App() {
   return (
     <div className='App'>
+      <Card />
       <Test />
     </div>
   );
---
+++ src/Testing/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <section>
+      <img src='' alt='' />
+      <h1>Temitope Agbowuro</h1>
+      <div>
+        <img src='' alt='' />
+        <p>08069477780</p>
+      </div>
+      <div>
+        <img src='' alt='' />
+        <p>12345678910</p>
+      </div>
+      <div></div>
+    </section>
+  );
+};
+
+export default Card;
