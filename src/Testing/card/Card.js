1a7f502a1b5d0e806e6f829b546367e1b45a2c84
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-08-29 20:46:33 +0100

    react sprint

--- src/Testing/card/Card.js
+++ src/Testing/card/Card.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Card = ({ phrase, num, date }) => {
+const Card = ({ name, phrase, num, date }) => {
   return (
     <div>
       <h1>{phrase}</h1>
       <h3>{num}</h3>
       <h3>{date}</h3>
+      <h1>{name}</h1>
     </div>
   );
 };
--- src/Testing/card/CardList.js
+++ src/Testing/card/CardList.js
@@ -1,19 +1,30 @@
 import React from "react";
 import Card from "./Card";
+import "./card.css";
 
-const details = {
-  phrase: "halleluya",
-};
+const details = [
+  {
+    phrase: "halleluya",
+    name: "ayo",
+    date: "5TH October,19XX",
+  },
+  {
+    name: "Adebayo",
+    date: "7th july",
+  },
+];
 
+// these are the props ==>
+// name, phrase, num, date
 const CardList = () => {
   const cardList = details.map((item) => {
     return (
       <div>
-        <Card phrase={item.phrase} />
+        <Card phrase={item.phrase} name={item.name} date={item.date} />
       </div>
     );
   });
-  return <div>{cardList}</div>;
+  return <div className='card'>{cardList}</div>;
 };
 
 export default CardList;
