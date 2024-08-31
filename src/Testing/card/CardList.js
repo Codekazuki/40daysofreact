f22ed3c577cf87fa56b69b349652a2590049cc6b
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-08-31 20:03:26 +0100

    data update

--- src/Testing/card/CardList.js
+++ src/Testing/card/CardList.js
@@ -1,41 +1,9 @@
 import React from "react";
 import Card from "./Card";
-
-const details = [
-  {
-    phrase: "halleluya",
-    name: "ayo",
-    date: "5TH October,19XX",
-    num: "8",
-  },
-  {
-    phrase: "Hossana",
-    num: "8",
-    name: "Adedayo",
-    date: "17th june",
-  },
-  {
-    phrase: "Rejoice",
-    num: "4",
-    name: "Stephen",
-    date: "4th may",
-  },
-  {
-    phrase: "Divine",
-    num: "10",
-    name: "Adeola",
-    date: "27th august",
-  },
-  {
-    phrase: "Peace",
-    num: "5",
-    name: "Temitops",
-    date: "5th May",
-  },
-];
+import { arrayOfObjects } from "./birthdayData";
 
 const CardList = () => {
-  const cardList = details.map((item) => {
+  const cardList = arrayOfObjects.map((item) => {
     return (
       <div>
         <Card
---
+++ src/Testing/card/birthdayData.js
@@ -0,0 +1,26 @@
+export const arrayOfObjects = [
+  {
+    name: "Alice",
+    num: 1,
+    phrase: "The early bird catches the worm",
+    date: "2024-08-31",
+  },
+  {
+    name: "Bob",
+    num: 2,
+    phrase: "Actions speak louder than words",
+    date: "2024-09-01",
+  },
+  {
+    name: "Charlie",
+    num: 3,
+    phrase: "Every cloud has a silver lining",
+    date: "2024-09-02",
+  },
+  {
+    name: "Diana",
+    num: 4,
+    phrase: "A journey of a thousand miles begins with a single step",
+    date: "2024-09-03",
+  },
+];
