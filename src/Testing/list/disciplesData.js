2d879a64a18164cdada3631eb27b3dfce4a10b71
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-01 20:10:09 +0100

    setting up for monday

---
+++ src/Testing/list/disciplesData.js
@@ -0,0 +1,91 @@
+export const disciples = [
+  {
+    name: "Peter",
+    occupation: "Fisherman",
+    placeOfOrigin: "Bethsaida",
+    knownFor:
+      "Leader of the Apostles, denied Jesus three times, first Pope according to tradition",
+    mannerOfDeath: "Crucified upside down in Rome",
+  },
+  {
+    name: "James (son of Zebedee)",
+    occupation: "Fisherman",
+    placeOfOrigin: "Bethsaida",
+    knownFor: "First Apostle to be martyred, brother of John",
+    mannerOfDeath: "Beheaded by Herod Agrippa I in Jerusalem",
+  },
+  {
+    name: "John",
+    occupation: "Fisherman",
+    placeOfOrigin: "Bethsaida",
+    knownFor:
+      "The 'Beloved Disciple', wrote the Gospel of John, Revelation, and Epistles of John",
+    mannerOfDeath: "Died of natural causes in Ephesus",
+  },
+  {
+    name: "Andrew",
+    occupation: "Fisherman",
+    placeOfOrigin: "Bethsaida",
+    knownFor:
+      "Peter’s brother, first-called disciple, missionary work in Eastern Europe",
+    mannerOfDeath: "Crucified on an X-shaped cross in Greece",
+  },
+  {
+    name: "Philip",
+    occupation: "Fisherman",
+    placeOfOrigin: "Bethsaida",
+    knownFor: "Brought Nathanael to Jesus, missionary work in Asia Minor",
+    mannerOfDeath: "Crucified in Hierapolis",
+  },
+  {
+    name: "Bartholomew (Nathanael)",
+    occupation: "Unknown",
+    placeOfOrigin: "Cana in Galilee",
+    knownFor: "Missionary work in Armenia and India, flayed alive",
+    mannerOfDeath: "Flayed and beheaded in Armenia",
+  },
+  {
+    name: "Matthew",
+    occupation: "Tax Collector",
+    placeOfOrigin: "Capernaum",
+    knownFor: "Author of the Gospel of Matthew, formerly known as Levi",
+    mannerOfDeath: "Martyred in Ethiopia, details unclear",
+  },
+  {
+    name: "Thomas",
+    occupation: "Unknown",
+    placeOfOrigin: "Galilee",
+    knownFor:
+      "Doubted Jesus' resurrection until seeing Him, missionary work in India",
+    mannerOfDeath: "Speared to death in India",
+  },
+  {
+    name: "James (son of Alphaeus)",
+    occupation: "Unknown",
+    placeOfOrigin: "Galilee",
+    knownFor: "Known as James the Less, possibly related to Jesus",
+    mannerOfDeath: "Martyred in Egypt, stoned and clubbed to death",
+  },
+  {
+    name: "Thaddeus (Jude)",
+    occupation: "Unknown",
+    placeOfOrigin: "Galilee",
+    knownFor: "Author of the Epistle of Jude, missionary work in Mesopotamia",
+    mannerOfDeath: "Martyred in Persia, often depicted with a club",
+  },
+  {
+    name: "Simon the Zealot",
+    occupation: "Unknown",
+    placeOfOrigin: "Cana in Galilee",
+    knownFor: "Former member of the Zealots, missionary work in Persia",
+    mannerOfDeath: "Martyred in Persia, sawed in half according to tradition",
+  },
+  {
+    name: "Judas Iscariot",
+    occupation: "Treasurer",
+    placeOfOrigin: "Kerioth in Judea",
+    knownFor:
+      "Betrayed Jesus for 30 pieces of silver, leading to His crucifixion",
+    mannerOfDeath: "Suicide by hanging",
+  },
+];
