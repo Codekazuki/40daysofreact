472dab8bb995ca46a4dbe56fd3c3c8dd17b1ae31
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-02 18:13:26 +0100

    sprint

--- src/Testing/list/EuropeanTeams.js
+++ src/Testing/list/EuropeanTeams.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { europeanFootballTeams } from "./footballTeams";
+import Team from "./Team";
 
-const EuropeanTeams = () => {
-  return <div>EuropeanTeams</div>;
+const EuropeanTeams = (team) => {
+  const listOfEuropeanTeams = europeanFootballTeams.map((team) => {
+    const {
+      name,
+      league,
+      europeanTitles,
+      mostFamousCoach,
+      highestVictory,
+      highestDefeat,
+    } = team;
+    return (
+      <Team
+        name={name}
+        league={league}
+        europeanTitls={europeanTitles}
+        mostFamousCoach={mostFamousCoach}
+        highestVictory={highestVictory}
+        highestDefeat={highestDefeat}
+      />
+    );
+  });
+  return <div>{listOfEuropeanTeams}</div>;
 };
 
 export default EuropeanTeams;
---
+++ src/Testing/list/Team.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Team = ({
+  name,
+  league,
+  europeanTitls,
+  mostFamousCoach,
+  highestVictory,
+  highestDefeat,
+}) => {
+  return (
+    <div>
+      <h1>Name:{name}</h1>
+      <h1>Plays in :{league}</h1>
+      <h1>Has {europeanTitls} european titles</h1>
+      <h1>
+        the most famous coach of {name}is {mostFamousCoach}
+      </h1>
+      <h1>{highestVictory}</h1>
+      <h1>{highestDefeat}</h1>
+    </div>
+  );
+};
+
+export default Team;
