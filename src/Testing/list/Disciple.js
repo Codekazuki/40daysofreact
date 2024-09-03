d0fba7416d1c49d6ac08c94b99cfcf179ba5cabe
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-03 21:32:01 +0100

    sprint

--- src/Testing/list/Disciple.js
+++ src/Testing/list/Disciple.js
@@ -1,14 +1,19 @@
 import React from "react";
 
-const Disciple = (props) => {
-  const { name, occupation, knownFor, mannerOfDeath, placeOfOrigin } = props;
+const Disciple = ({
+  name,
+  occupation,
+  knownFor,
+  mannerOfDeath,
+  placeOfOrigin,
+}) => {
   return (
     <div className='disciple'>
       <h1>Name: {name}</h1>
       <h1>Occupation:{occupation}</h1>
       <h1>Place of origin: {placeOfOrigin}</h1>
       <h1>Known for: {knownFor}</h1>
-      <h1>Manner of death: {mannerOfDeath}</h1>
+      <h1>Manner of: {mannerOfDeath}</h1>
     </div>
   );
 };
