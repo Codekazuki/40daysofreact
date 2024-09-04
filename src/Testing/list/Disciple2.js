7e35ca986a88c0e671307989b8fb26f712674986
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-04 08:16:38 +0100

    sprint

--- src/Testing/list/Disciple2.js
+++ src/Testing/list/Disciple2.js
@@ -15,7 +15,33 @@ const Disciple2 = () => {
       />
     );
   });
-  return <div className='list'>{listOfDisciple2}</div>;
+  return (
+    <div className='list'>
+      <form action=''>
+        <h1>Disciples Form</h1>
+        <label htmlFor='name'>Name: </label>
+        <input type='text' name='name' placeholder='name' /> <br />
+        <label htmlFor='occupation'>occupation: </label>
+        <input type='text' name='occupation' placeholder='occupation' /> <br />
+        <label htmlFor='name'>Place Of Origin: </label>
+        <input
+          type='text'
+          name='Place Of Origigin'
+          placeholder='Place Of Origigin'
+        />{" "}
+        <br />
+        <label htmlFor='name'>Known For: </label>
+        <input type='text' name='Known For' placeholder='Known For' /> <br />
+        <label htmlFor='name'>Manner Of Death: </label>
+        <input
+          type='text'
+          name='Manner Of Death'
+          placeholder='Manner Of Death'
+        />
+      </form>
+      {listOfDisciple2}
+    </div>
+  );
 };
 
 export default Disciple2;
