4ebb345cd9268b7075d0ca0c51ac79fd6efd15a0
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-11 15:13:36 +0100

    sprint

--- src/reviews/RenderedReviews.js
+++ src/reviews/RenderedReviews.js
@@ -3,9 +3,31 @@ import Reviews from "./Reviews";
 import people from "./data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
+const checkIndex = (number) => {
+  if (number > people.length - 1) {
+    return 0;
+  }
+  if (number < 0) {
+    return people.length - 1;
+  }
+  return number;
+};
+
 const RenderedReviews = () => {
   const [index, setIndex] = useState(0);
-  const { name, job, image, text } = people[0];
+  const { name, job, image, text } = people[index];
+  const prevPerson = () => {
+    setIndex((c) => {
+      const newIndex = c - 1;
+      return checkIndex(newIndex);
+    });
+  };
+  const nextPerson = () => {
+    setIndex((c) => {
+      const newIndex = c + 1;
+      return checkIndex(newIndex);
+    });
+  };
 
   return (
     <main>
@@ -19,11 +41,15 @@ const RenderedReviews = () => {
         <h4 className='author'>{name}</h4>
         <h4 className='job'>{job}</h4>
         <h4 className='info'>{text}</h4>
+        <div className='btn-container'>
+          <button onClick={prevPerson} className='prev-btn'>
+            <FaChevronLeft />
+          </button>
+          <button className='next-btn' onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
       </article>
-
-      <FaChevronLeft />
-      <FaChevronRight />
-      <Reviews />
     </main>
   );
 };
