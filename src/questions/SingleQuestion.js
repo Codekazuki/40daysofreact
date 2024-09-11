ead4a68116f470d4aa9d926737c1169f988e7abc
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-11 20:47:29 +0100

    sprint

--- src/questions/SingleQuestion.js
+++ src/questions/SingleQuestion.js
@@ -3,12 +3,23 @@ import { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 const SingleQuestion = ({ title, info }) => {
   const [showInfo, setShowInfo] = useState(false);
+  //   const toggleShow = () => {
+  //     const toggle = showInfo ? true : false;
+  //     setShowInfo(toggle);
+  //   };
   return (
     <article className='question'>
       <header>
         <h5>{title}</h5>
+        <span>
+          {showInfo ? (
+            <AiOutlineMinus onClick={() => setShowInfo(!showInfo)} />
+          ) : (
+            <AiOutlinePlus onClick={() => setShowInfo(!showInfo)} />
+          )}
+        </span>
       </header>
-      <p>{info}</p>
+      {showInfo && <p>{info}</p>}
     </article>
   );
 };
