e88845e04225b10bb6376411c601b24497213588
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-11 18:03:46 +0100

    sprint

--- src/questions/RenderedQuestion.js
+++ src/questions/RenderedQuestion.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data";
+import Questions from "./Questions";
 
 const RenderedQuestion = () => {
-  return <div>RenderedQuestion</div>;
+  const [questions, setQuestions] = useState(data);
+
+  return (
+    <main>
+      <Questions questions={questions} />
+    </main>
+  );
 };
 
 export default RenderedQuestion;
