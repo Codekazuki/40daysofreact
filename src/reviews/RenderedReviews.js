60dfe6e472a93ff376ac247529e892b7f52fdfd4
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-11 13:35:47 +0100

    sprint

--- src/reviews/RenderedReviews.js
+++ src/reviews/RenderedReviews.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Reviews from "./Reviews";
+import people from "./data";
 
 const RenderedReviews = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[0];
+
   return (
     <div>
       <Reviews />
