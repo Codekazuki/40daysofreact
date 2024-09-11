0dcf5c10dc2981f9d73f3bc86b87e9ba802630c1
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-11 06:39:16 +0100

    sprint

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Disciple2 from "./Testing/list/Disciple2";
 import Disciples from "./Testing/list/Disciples";
 import EuropeanTeams from "./Testing/list/EuropeanTeams";
 import RenderedBirthday from "./Testing/RenderedBirthday";
+import Tour from "./tours/Tour";
 
 function App() {
   return (
     <div className='App'>
-      <BirthdayState />
-      <Disciple2 />
+      <Tour />
+      {/* <BirthdayState /> */}
+      {/* <Disciple2 /> */}
       {/* <Disciples /> */}
       {/* <EuropeanTeams /> */}
       {/* <CardList /> */}
