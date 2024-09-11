53272ba27cfc2dc21d113c12d88e17dff1032d08
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-11 10:24:22 +0100

    sprint

--- src/tours/Tour.js
+++ src/tours/Tour.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
 const Tour = ({ id, image, info, name, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <div className='tour'>
       <img className='tour-image' src={image} alt={name} />
       <span className='tour-price'>${price}</span>
       <div>
         <h5 className='tour-name'>{name}</h5>
-        <p className='tour-info'>{info}</p>
+        <p className='tour-info'>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button
+            onClick={() => {
+              setReadMore(!readMore);
+            }}
+            className='btn'
+          >
+            continue reading
+          </button>
+        </p>
       </div>
-      <button className='btn'>continue reading</button>
+
       <button onClick={() => removeTour(id)}>RemoveTour</button>
     </div>
   );
