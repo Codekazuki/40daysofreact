5a9fc839f90cb01a8f2cfac60163e77e989e82f9
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-11 08:18:43 +0100

    sprint

--- src/tours/RenderedTours.js
+++ src/tours/RenderedTours.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Tours from "./Tours";
+
+const url = "https://www.course-api.com/react-tours-project";
 
 const RenderedTours = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const resp = await fetch(url);
+      const tours = await resp.json();
+      console.log(tours);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
   return (
     <div>
       <Tours />
