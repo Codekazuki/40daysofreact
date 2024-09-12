32969aea6f64674fa4e9a92f1636181f7bf1f4ec
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-12 14:24:27 +0100

    sprint

--- src/tabs/RenderedTabs.js
+++ src/tabs/RenderedTabs.js
@@ -1,8 +1,26 @@
-import React from "react";
-const url = "https://course-api.com/react-tabs-project";
+import React, { useEffect, useState } from "react";
+const url = "https://www.course-api.com/react-tabs-project";
 
 const RenderedTabs = () => {
-  return <div>RenderedTabs</div>;
+  const [tabs, setTabs] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h2>hello</h2>
+    </div>
+  );
 };
 
 export default RenderedTabs;
