caf2cfcf06e5ee1d53ede18026b9a4ed62fcc643
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-12 11:03:58 +0100

    sprint

--- src/menu/MenuItem.js
+++ src/menu/MenuItem.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-const MenuItem = ({ title, category, price, img, desc }) => {
-  console.log(img);
+const MenuItem = ({ title, price, img, desc }) => {
   return (
     <article className='menu-item'>
-      <img src={img} alt={title} />
+      <img src={img} alt={title} className='photo' />
+      <div className='item-info'>
+        <header>
+          <h4 className='price'>${price}</h4>
+          <h4>{title}</h4>
+        </header>
+        <p className='item-text'>{desc}</p>
+      </div>
     </article>
   );
 };
