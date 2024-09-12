d3a924a5936802b549e63b5ecb3fe982e7e8cdb2
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-12 11:51:20 +0100

    sprint

--- src/menu/Categories.js
+++ src/menu/Categories.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-const Categories = ({ categories }) => {
+const Categories = ({ categories, filterItems }) => {
   return (
     <div className='btn-container'>
       {categories.map((category) => {
         return (
-          <button type='button' className='filter-btn' key={category}>
+          <button
+            onClick={() => filterItems(category)}
+            type='button'
+            className='filter-btn'
+            key={category}
+          >
             {category}
           </button>
         );
--- src/menu/RenderedMenu.js
+++ src/menu/RenderedMenu.js
@@ -9,10 +9,19 @@ const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 const RenderedMenu = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   return (
     <main>
       <Title text='Our Menu' />
-      <Categories categories={categories} />
+      <Categories filterItems={filterItems} categories={categories} />
       <Menu items={menuItems} />
     </main>
   );
