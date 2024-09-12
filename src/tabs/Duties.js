d24817dc96d8088fd500dc5804d695ed4d1eb1cf
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-12 15:17:58 +0100

    sprint

--- src/tabs/Duties.js
+++ src/tabs/Duties.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
 const Duties = ({ duties }) => {
-  console.log(duties);
-  return <div>Duties</div>;
+  return (
+    <div>
+      {duties.map((duty, index) => {
+        return (
+          <div key={index} className='job-desc'>
+            <FaAngleDoubleRight className='job-icon' />
+            <p>{duty} </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Duties;
