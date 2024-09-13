664ee0b3b782d010c32b54c6b76660b53c2b14f7
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-13 16:48:41 +0100

    sprint

--- src/slider/Carousel.js
+++ src/slider/Carousel.js
@@ -6,21 +6,35 @@ import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 console.log(shortList);
 const Carousel = () => {
   const [people, setPeople] = useState(longList);
+  const [currPerson, setCurrPerson] = useState(0);
 
   const nextSlide = () => {
-    console.log("next");
+    setCurrPerson((c) => {
+      const result = (c + 1) % people.length;
+      return result;
+    });
   };
   const prevSlide = () => {
-    console.log("prev");
+    setCurrPerson((c) => {
+      const result = (c - 1 + people.length) % people.length;
+      return result;
+    });
   };
 
   return (
     <div className='slider-container'>
-      {people.map((person) => {
+      {people.map((person, personIndex) => {
         const { id, image, name, quote, title } = person;
         console.log(person);
         return (
-          <article className='slider next-slide' key={id}>
+          <article
+            className='slider'
+            style={{
+              transform: `translateX(${100 * (personIndex - currPerson)}%)`,
+              opacity: personIndex === currPerson ? 1 : 0,
+            }}
+            key={id}
+          >
             <img src={image} alt={name} className='person-img' />
             <h5 className='name'>{name}</h5>
             <p className='title'>{title}</p>
