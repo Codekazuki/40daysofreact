35c884d87909c55c78822fd118e5f6cbd4a0002f
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-18 10:35:43 +0100

    sprint

--- src/grocerybud/GroceryBud.js
+++ src/grocerybud/GroceryBud.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GroceryBud = () => {
-  return <div>GroceryBud</div>;
+  const [items, setItems] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+  };
+  return (
+    <section>
+      <h1>Grocery Board</h1>
+      <form>
+        <label htmlFor='item' name='item'>
+          Item
+        </label>
+        <input
+          placeholder='input your item'
+          id='item'
+          value={items}
+          onChange={(e) => e.target.value}
+          type='text'
+        />
+        <button type='submit' onSubmit={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default GroceryBud;
