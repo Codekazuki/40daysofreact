406d606026db39dcf48c2147275cde380bad0a4b
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-20 14:25:37 +0100

    sprint

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -7,23 +7,32 @@ const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
   const [showIcons, setShowIcons] = useState(false);
   const [showLogo, setShowLogo] = useState(false);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
   return (
     <div>
       <nav className='nav-center'>
         <div className='nav-header'>
-          <img src={logo} alt='logo' />
+          <img src={logo} alt='logo' className='logo' />
           <button className='nav-toggle'>
-            <FaBars />
+            <FaBars onClick={toggleLinks} />
           </button>
         </div>
-        <div className='links-container'>
-          <ul className='links'>
-            {links.map((link) => {
-              const { id, url, text } = link;
-              return <li key={id}>{text}</li>;
-            })}
-          </ul>
-        </div>
+        {showLinks && (
+          <div className='links-container'>
+            <ul className='links'>
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
       </nav>
     </div>
   );
