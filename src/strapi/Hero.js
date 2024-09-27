78659da50ef43ad31f4fa5d082af80ccfb7930b8
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-27 08:10:59 +0100

    sprint

--- src/strapi/Hero.js
+++ src/strapi/Hero.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Hero = () => {
-  return <div className='hero-container'>Hero</div>;
+  return (
+    <div className='hero-container'>
+      <div className='hero-center'>
+        <h1>
+          {" "}
+          The Hero Content <br /> here{" "}
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Distinctio quasi
+          voluptate laboriosam.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/strapi/SideBar.js
+++ src/strapi/SideBar.js
@@ -2,22 +2,29 @@ import React from "react";
 import { useGlobalContext } from "./context";
 import { FaBars } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
+import sublinks from "./data";
 
 const SideBar = () => {
   const { isSideBarOpen, openSideBar, closeSideBar } = useGlobalContext();
   return (
-    <section
-      className={isSideBarOpen ? "sidebar-wrapper.show" : "sidebar-wrapper"}
-    >
+    // <aside className='sidebar'>
+    <div className={isSideBarOpen ? "sidebar-wrapper.show" : "sidebar-wrapper"}>
       {isSideBarOpen && (
         <div className='sidebar-wrapper.show'>
           <h1>This is the SideBar</h1>
           <button onClick={closeSideBar} className='close-btn'>
             <IoMdClose />
           </button>
+          <div className='sidebar-links'>
+            {sublinks.map((item) => {
+              console.log(item);
+              return <h1>item</h1>;
+            })}
+          </div>
         </div>
       )}
-    </section>
+    </div>
+    // </aside>
   );
 };
 
