754b8a60d74d9298e24b6decd7c63cbe8579b0de
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-27 11:40:24 +0100

    sprint

--- src/strapi/NavBar.js
+++ src/strapi/NavBar.js
@@ -4,17 +4,21 @@ import { FaBars } from "react-icons/fa";
 import NavLinks from "./NavLinks";
 
 const NavBar = () => {
-  const { openSideBar } = useGlobalContext();
-
+  const { openSideBar, setPageId } = useGlobalContext();
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains("nav-link")) {
+      setPageId(null);
+    }
+  };
   return (
-    <nav>
+    <nav onMouseOver={handleSubmenu}>
       <div className='nav-center'>
         <h3 className='logo'>The Nav</h3>
         <button className='btn toggle-btn' onClick={openSideBar}>
           <FaBars />
         </button>
+        <NavLinks />
       </div>
-      <NavLinks />
     </nav>
   );
 };
--- src/strapi/SubMenu.js
+++ src/strapi/SubMenu.js
@@ -3,14 +3,19 @@ import { useGlobalContext } from "./context";
 import sublinks from "./data";
 
 const SubMenu = () => {
-  const { pageId } = useGlobalContext();
+  const { pageId, setPageId } = useGlobalContext();
   const currentPage = sublinks.find((item) => item.pageId === pageId);
-
+  const handleMouseLeave = (event) => {
+    setPageId(null);
+  };
   return (
-    <div className='submenu'>
+    <div
+      className={currentPage ? "submenu show-submenu" : "submenu"}
+      onMouseLeave={handleMouseLeave}
+    >
       <h5>{currentPage?.page}</h5>
       <div
-        className='sub-menu-links'
+        className='submenu-links'
         style={{
           gridTemplateColumns:
             currentPage?.links?.length > 3 ? "1fr 1fr" : "1fr",
