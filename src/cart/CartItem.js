0cc2f9cef5966491c9d14f8158c6d8903688eb53
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-01 21:51:34 +0100

    sprint

--- src/cart/CartItem.js
+++ src/cart/CartItem.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import cartItems from "./data";
 
 const CartItem = () => {
+  const [data, setData] = useState(cartItems);
+  const handleDelete = (id) => {
+    const newList = data.filter((sorted) => sorted.id !== id);
+    setData(newList);
+    console.log(id);
+  };
   return (
     <div>
-      {cartItems.map((item) => {
+      {data.map((item) => {
         const { id, title, price, amount, img } = item;
         return (
-          <ul>
+          <ul key={id}>
             <li>
               <span>{amount}</span>
               <h3>{title}</h3>
               <span>{price}</span>
               <img src={img} alt={title} style={{ height: "4rem" }} />
+              <button onClick={() => handleDelete(id)} className='btn'>
+                delete
+              </button>
             </li>
           </ul>
         );
       })}
+
+      {data.length === 0 ? (
+        <>
+          <h4>The List is empty at the moment</h4>
+          <button className='btn' onClick={() => setData(cartItems)}>
+            Fetch List Again
+          </button>
+        </>
+      ) : (
+        <button className='btn' onClick={() => setData([])}>
+          ClearList
+        </button>
+      )}
     </div>
   );
 };
