2d63d02a74c88c659901552353506a31ee6447cf
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-01 21:18:29 +0100

    sprint

--- src/cart/CartItem.js
+++ src/cart/CartItem.js
@@ -1,7 +1,24 @@
 import React from "react";
+import cartItems from "./data";
 
 const CartItem = () => {
-  return <div>CartItem</div>;
+  return (
+    <div>
+      {cartItems.map((item) => {
+        const { id, title, price, amount, img } = item;
+        return (
+          <ul>
+            <li>
+              <span>{amount}</span>
+              <h3>{title}</h3>
+              <span>{price}</span>
+              <img src={img} alt={title} style={{ height: "4rem" }} />
+            </li>
+          </ul>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CartItem;
