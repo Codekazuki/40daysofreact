a444f7ab387506dfc3d08d29ec730389449fe260
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-01-18 12:00:00 +0100

    Your commit message

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
         <RenderedSidebar />
       </AppProvider> */}
       {/* <RenderedNavbar /> */}
-      {/* <GroceryBud /> */}
+      <GroceryBud />
       {/* <ColorGenerator /> */}
       {/* <Lorem /> */}
       {/* <Slider /> */}
