310d8801a54c169a7e8f6621bc89ba374fc5b67b
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-02-22 22:01:19 +0100

    sprint

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./index.css";
 
 const ProgressBar = ({ progress }) => {
   return (
-    <div>
-      <div>{progress}</div>
+    <div className='outer'>
+      <div style={{ width: `${progress}%` }} className='inner'>
+        {progress}%
+      </div>
     </div>
   );
 };
